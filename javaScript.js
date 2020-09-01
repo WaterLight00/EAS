44d5687c2572ee0ec4d3d5d62f1aca9ddb0a9249
wilmer <wilmer.rw@gmail.com>
2020-09-01 07:51:16 -0400

    added coloring functionality and a border to define the coloring space

--- javaScript.js
+++ javaScript.js
@@ -3,10 +3,13 @@ let numberOfBoxes = prompt("specify number of boxes");
 function createGridElemnts(numberOfBoxes){
     let totalBoxes = numberOfBoxes**2;
     const container = document.querySelector('.container');
-    for (let index = 1; index < totalBoxes; index++) {
+    for (let index = 0; index < totalBoxes; index++) {
         const box = document.createElement('div');
         box.classList.add('box')
-        box.textContent = " .";
+        box.textContent = ".";
+        box.classList.add('boxColor');
+        // box.style['background-color']="white";
+        // box.style['color']="white";
         container.appendChild(box);
         //box.style['gridColumn']=index+'/'+index;
         //box.style['gridRow']=`${index}/${index}`;
@@ -20,6 +23,9 @@ function changeBoxColor(){
     boxes.forEach((box) => {
 
         box.addEventListener('mouseover', function (e){
+            box.classList.remove('boxColor');
+            // box.removeAttribute('color');
+            // box.removeAttribute('background-color');
             box.classList.add('colored');
         
     });
