65b6851a89d70f17370b277f85925635c004b778
wilmer <wilmer.rw@gmail.com>
2020-09-01 06:51:37 -0400

    remove functionality :(

---
+++ javaScript.js
@@ -0,0 +1,30 @@
+let numberOfBoxes = prompt("specify number of boxes");
+
+function createGridElemnts(numberOfBoxes){
+    let totalBoxes = numberOfBoxes**2;
+    const container = document.querySelector('.container');
+    for (let index = 1; index < totalBoxes; index++) {
+        const box = document.createElement('div');
+        box.classList.add('box')
+        box.textContent = index;
+        container.appendChild(box);
+        box.style['gridColumn']=index+'/'+index;
+        box.style['gridRow']=`${index}/${index}`;
+    }
+};
+createGridElemnts(numberOfBoxes);
+
+function changeBoxColor(){
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach((box) => {
+
+        box.addEventListener('mouseover', function (e){
+            box.classList.add('colored');
+        
+    });
+});
+}
+
+changeBoxColor();
+
